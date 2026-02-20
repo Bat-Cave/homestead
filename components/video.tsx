20af22acde982d6a4dcbfe2f07eaa48783e51585
Rico Hancock <ricohancock@proton.me>
2026-02-19 17:04:18 -0700

    feat: :sparkles: Enhance Video component by adding accentColor and preferPlayback props, and refactor video rendering to use MuxPlayer for improved playback experience

--- components/video.tsx
+++ components/video.tsx
@@ -4,27 +4,39 @@ import { cn } from "@/lib/utils";
 
 export default function Video({
 	className,
+	accentColor = "#a684ff",
+	preferPlayback = "native",
 	...props
 }: ComponentProps<typeof MuxPlayer>) {
 	return (
 		<div className={cn("w-full h-auto rounded overflow-hidden my-2")}>
-			<video
+			<style
+				dangerouslySetInnerHTML={{
+					__html: `
+						mux-player {
+							--media-object-fit: contain;
+							--media-object-position: center;
+						}
+					`,
+				}}
+			/>
+			{/* <video
 				src={`https://stream.mux.com/${props.playbackId}.m3u8`}
 				className={cn(
 					"w-full flex items-center justify-center max-h-[calc(100dvh-88px)] object-cover",
 					className,
 				)}
 				controls
-			/>
-			{/* <MuxPlayer
+			/> */}
+			<MuxPlayer
 				accentColor={accentColor}
+				preferPlayback={preferPlayback}
 				{...props}
-				debug
 				className={cn(
 					"w-full flex items-center justify-center max-h-[calc(100dvh-88px)] aspect-9/16",
 					className,
 				)}
-			/> */}
+			/>
 		</div>
 	);
 }
